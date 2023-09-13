213255a81d50f629a4666f5e58f975119e9cfab2
Sonja Feitsch <feitsch@e2n.de>
2023-09-13 11:42:23 +0200

    handle resize of uploaded file

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,26 +1,55 @@
-import { ActionArgs, LoaderArgs, json } from "@remix-run/node";
 import { Form } from "@remix-run/react";
 import { exec } from "node:child_process";
 import { Button, TextField } from "~/components";
+import {
+  type ActionArgs,
+  unstable_composeUploadHandlers,
+  unstable_createFileUploadHandler,
+  unstable_createMemoryUploadHandler,
+  unstable_parseMultipartFormData,
+} from "@remix-run/node";
 
 export const action = async ({ request }: ActionArgs) => {
-  const formData = await request.formData();
-  const file = formData.get("file");
+  const uploadHandler = unstable_composeUploadHandlers(
+    unstable_createFileUploadHandler({
+      maxPartSize: 5_000_000,
+      file: ({ filename }) => filename,
+    }),
+    unstable_createMemoryUploadHandler()
+  );
+
+  const formData = await unstable_parseMultipartFormData(
+    request,
+    uploadHandler
+  );
+
+  const file = formData.get("file") as any;
   const scale = formData.get("scale");
-  exec(`magick ${file} -resize ${scale} resized.jpg`, (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`stdout: ${stdout}`);
-    }
-  });
+
+  if (file) {
+    exec(
+      `magick ${file.filepath} -resize ${scale} resized.jpg`,
+      (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`stdout: ${stdout}`);
+        }
+      }
+    );
+  }
   return null;
 };
 
 export default function Index() {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <Form action="/?index" method="post" className="flex flex-col gap-2">
+      <Form
+        encType="multipart/form-data"
+        action="/?index"
+        method="post"
+        className="flex flex-col gap-2"
+      >
         <TextField
           onChange={(e) => console.log(e.target.files)}
           label="Datei"
