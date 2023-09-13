15430a7f9b476a7068fe27299a50eeea8acdeb59
Sonja Feitsch <feitsch@e2n.de>
2023-09-13 11:10:00 +0200

    add form and call imagemagick

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,35 @@
-import { LoaderArgs, json } from "@remix-run/node";
+import { ActionArgs, LoaderArgs, json } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { exec } from "node:child_process";
+import { Button, TextField } from "~/components";
 
-// export const loader = async ({ request }: LoaderArgs) => {};
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+  const file = formData.get("file");
+  const scale = formData.get("scale");
+  exec(`magick ${file} -resize ${scale} resized.jpg`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`stdout: ${stdout}`);
+    }
+  });
+  return null;
+};
 
 export default function Index() {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      Transform your images
+      <Form action="/?index" method="post" className="flex flex-col gap-2">
+        <TextField
+          onChange={(e) => console.log(e.target.files)}
+          label="Datei"
+          name="file"
+          type="file"
+        />
+        <TextField label="Skalierung" name="scale" />
+        <Button type="submit">Skalieren</Button>
+      </Form>
     </div>
   );
 }
